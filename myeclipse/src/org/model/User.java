49629bd02c362a44c1b8e60252e16e6763df3abd
mlawp <15913136993@qq.com>
2015-03-08 21:41:36 +0800

    搭建环境,写了个User类

---
+++ myeclipse/src/org/model/User.java
@@ -0,0 +1,19 @@
+package org.model;
+
+public class User {
+	private int id;	
+	private String pass;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+}
